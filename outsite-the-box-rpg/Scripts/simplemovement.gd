235649e63a0913fdc320806abdcc879ec14c211e
Famedtube <gearsofwar2289@gmail.com>
2026-03-03 19:45:02 -0500

    basic movement done
    The movement is done. You can test it out be just pressing play. please send feedback on the speed and it it should be slower or faster.

---
+++ outsite-the-box-rpg/Scripts/simplemovement.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+# can change speed if too slow or fast but this is just a place holder
+@export var speed = 530
+# this code is just simple movement that let you move the charcter 8 way so you can use two keys to move diagonanly
+func get_input():
+	var input_direction = Input.get_vector("movement_left","movement_right","movement_up","movement_down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
