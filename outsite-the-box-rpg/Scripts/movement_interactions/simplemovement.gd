cb2f6d43b192d80eb81612d18776d428981692fe
Famedtube <gearsofwar2289@gmail.com>
2026-03-05 09:27:49 -0500

    added a scene to test stuff
    The scene is just so I can test some interactions before we add the main part of the game, so when we can start working on how the map looks and stuff

--- outsite-the-box-rpg/Scripts/movement_interactions/simplemovement.gd
+++ outsite-the-box-rpg/Scripts/movement_interactions/simplemovement.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 # can change speed if too slow or fast but this is just a place holder
-@export var speed = 530
+@export var speed = 400
 # this code is just simple movement that let you move the charcter 8 way so you can use two keys to move diagonanly
 func get_input():
 	var input_direction = Input.get_vector("movement_left","movement_right","movement_up","movement_down")
