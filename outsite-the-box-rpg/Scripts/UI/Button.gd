d6f009839b5a671e744cb77fb917597ce400a13d
TheLastPear <bleickman7@gmail.com>
2026-03-04 23:10:07 -0600

    Battle UI base finished
    Finished the base for the battle ui. The navigation is there, but not the functionality.

--- outsite-the-box-rpg/Scripts/UI/Button.gd
+++ outsite-the-box-rpg/Scripts/UI/Button.gd
@@ -1,15 +1,13 @@
 extends Button
 
+@onready var on_focused = $"/root/Battle/UI Audio/main"
+
 @export var start_with_focus : bool
-signal on_pressed
 
 
 func _ready() -> void:
+	focus_exited.connect(on_focused.play)
+	
 	if is_visible_in_tree() and start_with_focus:
 		grab_focus.call_deferred()
-	pass
-
-
-func _pressed() -> void:
-	on_pressed.emit(self)
 	pass
--- outsite-the-box-rpg/Scripts/UI/UI Window.gd
+++ outsite-the-box-rpg/Scripts/UI/UI Window.gd
@@ -1,35 +1,37 @@
 extends Control
 
-var focus_memory : Button
+@onready var on_back = $"/root/Battle/UI Audio/back"
 
+@export var previous_window : Control
+@export var first_focus : Control
+signal on_return
 
 func _ready() -> void:
-	
+	on_return.connect(on_back.play)
+	if previous_window != null:
+		on_return.connect(previous_window.open_window)
 	pass
 
 
-func open_window(from : Button) -> void:
-	focus_memory = from
-	
+func open_window() -> void:
 	self.show()
-	
-	for child in get_children():
-		if child is Button:
-			var button = child as Button
-			button.grab_focus()
-			print("Focus grabbed")
-			break
-	
+	if first_focus != null:
+		first_focus.grab_focus()
 	pass
 
 
 func return_to_other_window() -> void:
 	self.hide()
-	
-	focus_memory.grab_focus()
+	on_return.emit()
 	pass
 
 
 func next_window() -> void:
 	self.hide()
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel") and previous_window != null and is_visible_in_tree():
+		return_to_other_window()
 	pass
